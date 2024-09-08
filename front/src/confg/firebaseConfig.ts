b2fb748a5f0937a38e3d1e0b9843b7d1383b97b1
dorlevy <49606382+dorlevy1@users.noreply.github.com>
2024-09-08 05:36:36 +0300

    .

--- front/src/confg/firebaseConfig.ts
+++ front/src/confg/firebaseConfig.ts
@@ -4,13 +4,13 @@ import 'firebase/compat/storage';
 import 'firebase/compat/functions';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBvKq6rpRdOcZF3pzCpcd3oDHXutjOkV5Y",
-    authDomain: "cicilia-e6f75.firebaseapp.com",
-    projectId: "cicilia-e6f75",
-    storageBucket: "cicilia-e6f75.appspot.com",
-    messagingSenderId: "68302801528",
-    appId: "1:68302801528:web:ba6e7d3eef6baf3806bb8d",
-    measurementId: "G-M2FK8ZPZF0"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = firebase.initializeApp(firebaseConfig);
